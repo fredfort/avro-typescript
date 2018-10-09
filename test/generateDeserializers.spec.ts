7fb6e43f07a3676e820690bea14355ae84b7e293
Balazs Edes <balazs.edes@workday.com>
2018-10-09 14:24:26 +0100

    Added deserializer generator tess

--- test/generateDeserializers.spec.ts
+++ test/generateDeserializers.spec.ts
@@ -3,32 +3,32 @@ import { getSchema } from './utils/fileUtils'
 import { generateInterface } from '../src/generators/generateInterface'
 import { generateAvroWrapper } from '../src/generators/generateAvroWrapper'
 import { generateFqnConstant } from '../src/generators/generateFqnConstants'
-import { generateSerialize } from '../src/generators/generateSerialize'
+import { generateDeserialize } from '../src/generators/generateDeserialize'
 import { generateTypeGuard } from '../src/generators/generateTypeGuard'
 import { RootTypeContext } from '../src/common/RootTypeContext'
 import { TestCompilerHelper } from './utils/TestCompilerHelper'
 
-describe('Generate Serializers', () => {
+describe('Generate Deserializers', () => {
   describe('PrimitiveProps.avsc', () => {
     const schema = getSchema('PrimitiveProps')
     const context = new RootTypeContext([{ filename: 'PrimitiveProps', rootType: schema }])
     const record = context.getRecordType('PrimitiveProps')
     const sourceCode = `${generateAvroWrapper(record, context)}
       ${generateInterface(record, context)}
-      ${generateSerialize(record, context)}`
+      ${generateDeserialize(record, context)}`
 
     const t = new TestCompilerHelper(sourceCode)
 
-    it('should have generated the right interfaces and serializePrimitiveProps function', () => {
+    it('should have generated the right interfaces and deserializePrimitiveProps function', () => {
       expect(t.getInterface('IPrimitiveProps')).to.be.an('object')
       expect(t.getInterface('IPrimitivePropsAvroWrapper')).to.be.an('object')
-      expect(t.getFunction('serializePrimitiveProps')).to.be.an('object')
+      expect(t.getFunction('deserializePrimitiveProps')).to.be.an('object')
     })
 
-    it('should have generated serializePrimitiveProps with the proper signature', () => {
-      const fn = t.getFunction('serializePrimitiveProps')
-      t.expectParameterTypes(t.expectTypeReference('IPrimitiveProps'))(fn)
-      t.expectTypeReference('IPrimitivePropsAvroWrapper')(fn.type)
+    it('should have generated deserializePrimitiveProps with the proper signature', () => {
+      const fn = t.getFunction('deserializePrimitiveProps')
+      t.expectParameterTypes(t.expectTypeReference('IPrimitivePropsAvroWrapper'))(fn)
+      t.expectTypeReference('IPrimitiveProps')(fn.type)
     })
 
     xit('should test for generated body', () => {
@@ -48,31 +48,31 @@ describe('Generate Serializers', () => {
       ${generateAvroWrapper(leaf, context)}
       ${generateInterface(leaf, context)}
       ${generateTypeGuard(leaf, context)}
-      ${generateSerialize(leaf, context)}
+      ${generateDeserialize(leaf, context)}
       ${generateAvroWrapper(tree, context)}
       ${generateInterface(tree, context)}
       ${generateTypeGuard(tree, context)}
-      ${generateSerialize(tree, context)}`
+      ${generateDeserialize(tree, context)}`
 
     const t = new TestCompilerHelper(sourceCode)
 
-    it('should have generated the right interfaces and serializeTree and serializeLeaf function', () => {
+    it('should have generated the right interfaces and deserializeTree and deserializeLeaf function', () => {
       expect(t.getInterface('ITree')).to.be.an('object')
       expect(t.getInterface('ITreeAvroWrapper')).to.be.an('object')
       expect(t.getInterface('ILeaf')).to.be.an('object')
       expect(t.getInterface('ILeafAvroWrapper')).to.be.an('object')
-      expect(t.getFunction('serializeTree')).to.be.an('object')
-      expect(t.getFunction('serializeLeaf')).to.be.an('object')
+      expect(t.getFunction('deserializeTree')).to.be.an('object')
+      expect(t.getFunction('deserializeLeaf')).to.be.an('object')
     })
 
-    it('should have generated serializeTree and serializeLeaf with the proper signature', () => {
-      const treeFn = t.getFunction('serializeTree')
-      t.expectParameterTypes(t.expectTypeReference('ITree'))(treeFn)
-      t.expectTypeReference('ITreeAvroWrapper')(treeFn.type)
+    it('should have generated deserializeTree and deserializeLeaf with the proper signature', () => {
+      const treeFn = t.getFunction('deserializeTree')
+      t.expectParameterTypes(t.expectTypeReference('ITreeAvroWrapper'))(treeFn)
+      t.expectTypeReference('ITree')(treeFn.type)
 
-      const leafFn = t.getFunction('serializeLeaf')
-      t.expectParameterTypes(t.expectTypeReference('ILeaf'))(leafFn)
-      t.expectTypeReference('ILeafAvroWrapper')(leafFn.type)
+      const leafFn = t.getFunction('deserializeLeaf')
+      t.expectParameterTypes(t.expectTypeReference('ILeafAvroWrapper'))(leafFn)
+      t.expectTypeReference('ILeaf')(leafFn.type)
     })
 
     xit('should test for generated body', () => {
